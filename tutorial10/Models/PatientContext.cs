10e99c868823f8bd56c85497912634339c261b6f
s18289 <s18289@pjwstk.edu.pl>
2020-05-22 14:05:44 +0200

    commit

---
+++ tutorial10/Models/PatientContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial10.Models
+{
+    public class PatientContext : DbContext
+    {
+        public virtual DbSet<Patient> Patient { get; set; }
+        public virtual DbSet<Doctor> Doctor { get; set; }
+        public virtual DbSet<Prescription> Prescription { get; set; }
+        public virtual DbSet<Medicament> Medicament { get; set; }
+        public virtual DbSet<Prescription_Medicament> Prescription_Medicament { get; set; }
+
+        public PatientContext() { }
+
+        public PatientContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ tutorial10/Models/Prescription_Medicament.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial10.Models
+{
+    public class Prescription_Medicament
+    {
+        [Key]
+        [ForeignKey("Medicament")]
+        public int IdMedicament { get; set; }
+
+        [Key]
+        [ForeignKey("Prescription")]
+        public int IdPrescription { get; set; }
+
+        public int? Dose { get; set; }
+        public string Details { get; set; }
+
+        public virtual Medicament IdMedicamentNavigation { get; set; }
+        public virtual Prescription IdPrescriptionNavigation { get; set; }
+    }
+}
